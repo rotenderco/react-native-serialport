6664cb8e86ff5e6ff4c2927950d5a428a456845f
Cory <cory@rotender.com>
2024-03-19 13:59:55 -0700

    using workaround to import usbserial.aar

--- android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportPackage.java
+++ android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportPackage.java
@@ -11,6 +11,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 
+import com.melihyarikkaya.rnserialport.RNSerialportModule;
+
 public class RNSerialportPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
