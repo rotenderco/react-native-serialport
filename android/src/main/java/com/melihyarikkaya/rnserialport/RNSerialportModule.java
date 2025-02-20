361b9f0a1b491910f7a7af48feee947ae3dfa02e
Cory <cory@rotender.com>
2025-02-20 12:27:45 -0800

    fix: add version checking for pendind intent flag

--- android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
+++ android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
@@ -567,9 +567,10 @@ public class RNSerialportModule extends ReactContextBaseJavaModule {
     if(device == null)
       return;
     Intent intent = new Intent(ACTION_USB_PERMISSION);
-    intent.putExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false);
+//    intent.putExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false);
     intent.setPackage(reactContext.getPackageName());
-    PendingIntent mPendingIntent = PendingIntent.getBroadcast(reactContext, 0, intent, PendingIntent.FLAG_MUTABLE);
+    int intentFlags = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) ? PendingIntent.FLAG_MUTABLE : 0;
+    PendingIntent mPendingIntent = PendingIntent.getBroadcast(reactContext, 0, intent, intentFlags);
     usbManager.requestPermission(device, mPendingIntent);
   }
 
